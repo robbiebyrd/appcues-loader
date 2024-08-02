073101785e57b6f9306dde9b723dc337c6fff8cc
Robbie Byrd <me@robbiebyrd.com>
2024-08-02 12:10:37 -0500

    Add some additional functions to the typescript definition

--- src/types.ts
+++ src/types.ts
@@ -8,9 +8,19 @@ type EventProperties = {
   [key: string]: ValueType;
 };
 
+type GroupProperties = {
+  [key: string]: ValueType;
+};
+
 export declare interface Appcues
 {
   identify: (userId: string, userProperties?: UserProperties) => void;
+  group: (userId: string, groupProperties?: GroupProperties) => void;
   page: () => void;
+  anonymous: () => void;
   track: (eventName: string, eventProperties?: EventProperties) => void;
+  show: (contentId: string) => void;
+  clearShow: () => void;
+  reset: () => void;
+  debug: (enable: boolean) => void;
 }
